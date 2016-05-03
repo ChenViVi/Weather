6e05c16254115426465a992eeaa43fe0fb995ad5
vivi <932942491@qq.com>
2016-05-03 21:38:42 +0800

    Initial commit

---
+++ app/src/main/java/chenyuwei/com/rnm/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,19 @@
+package chenyuwei.com.rnm.receiver;
+
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import chenyuwei.com.rnm.service.AutoUpdateService;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+	}
+
+}
---
+++ app/src/main/java/chenyuwei/com/rnm/util/HttpCallbackListener.java
@@ -0,0 +1,9 @@
+package chenyuwei.com.rnm.util;
+
+public interface HttpCallbackListener {
+
+	void onFinish(String response);
+
+	void onError(Exception e);
+
+}
